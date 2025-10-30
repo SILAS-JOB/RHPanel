92817e04147f4757f1fcda7ad1f90a52717d83c5
SILAS-JOB <tiilas777@gmail.com>
2025-10-30 20:12:36 -0300

    feat: middleware & setup swagger

---
+++ api/Models/Employer.cs
@@ -0,0 +1,10 @@
+namespace api.Models
+{
+    public class Employer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AdmissionDate { get; set; }
+        
+    }
+}
