69cd7c2c478a66d72c5a943f5747f976fc40c1fa
SILAS-JOB <tiilas777@gmail.com>
2025-11-06 12:17:20 -0300

    feat: HttpClient with fetch

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -3,9 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { withLatestFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(
+      withFetch()
+    ),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay())
