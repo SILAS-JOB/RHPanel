27044a0d30bed0b4da6edba99b3db0e343e07836
SILAS-JOB <tiilas777@gmail.com>
2025-11-06 12:16:46 -0300

    feat: http client & model

---
+++ web/src/app/services/funcionarios.service.ts
@@ -0,0 +1,23 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../pages/funcionarios.component/funcionarios.component';
+
+
+
+@Injectable({providedIn: 'root'})
+export class FuncionariosService {
+  private http = inject(HttpClient);
+
+  private apiUrl = 'http://localhost:5272/api/v1/user';
+
+
+  getItems(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.apiUrl);
+  }
+ 
+  createItem(item: Employee) : Observable<Employee> {
+    return this.http.post<Employee>(this.apiUrl, item)
+  } 
+
+}
