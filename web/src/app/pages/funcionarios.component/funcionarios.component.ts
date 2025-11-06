4acc5c9b779fc8836c9f02a89848d1d9837aee38
SILAS-JOB <tiilas777@gmail.com>
2025-11-06 12:16:14 -0300

    feat: start api request pull

--- web/src/app/pages/funcionarios.component/funcionarios.component.ts
+++ web/src/app/pages/funcionarios.component/funcionarios.component.ts
@@ -1,11 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FuncionariosService } from '../../services/funcionarios.service';
+import { CommonModule } from '@angular/common';
+
+export interface Employee {
+  id: number;
+  name: string;
+  admissionDate: Date;
+  salary: number;
+  isWorking: boolean;
+  demissionDate?: Date;
+  observation?: string;
+}
 
 @Component({
   selector: 'app-funcionarios',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './funcionarios.component.html',
-  styleUrl: './funcionarios.component.scss',
+  styleUrls: ['./funcionarios.component.scss'],
 })
-export class FuncionariosComponent {
+export class FuncionariosComponent implements OnInit {
+  employees: Employee[] = [];
+  loading = true;
+  error: string | null = null;
 
+  constructor(private funcionariosService : FuncionariosService ){}
+  ngOnInit(): void {
+    this.funcionariosService.getItems().subscribe({
+      next: (data: Employee[]) => {
+        this.employees = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = "Erro ao carregar empregados"
+        this.loading = false;
+        console.error('Erro carregando no fetch da api')
+      }
+    });
+    
+  }
 }
