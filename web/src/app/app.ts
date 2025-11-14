87c230f0a4c28615830e9da3e30996cb792e2fd5
SILAS-JOB <tiilas777@gmail.com>
2025-11-14 13:38:02 -0300

    feat: introcomponent

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./components/navbar.component/navbar.component"
 import { SidebarComponent } from "./components/sidebar.component/sidebar.component";
 import { FooterComponent } from "./components/footer.component/footer.component";
 import { FuncionariosComponent } from "./pages/funcionarios.component/funcionarios.component";
+import { IntroComponent } from "./pages/intro.component/intro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent, FuncionariosComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent, FuncionariosComponent, IntroComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
